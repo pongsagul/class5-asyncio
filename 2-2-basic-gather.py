652c844afb9d7ff604fff2cadf9accc84a51c235
pongsagul <6310301003@cdti.ac.th>
2023-07-26 14:52:40 +0700

    commit_1

--- 2-2-basic-gather.py
+++ 2-2-basic-gather.py
@@ -8,13 +8,13 @@ async def hello(i):
     print(f"{time.ctime()} hello {i} done")
 
 
-#สร้าง ฟังชั่น main เพื่อกำหนด ตัวแปร task  กับ 2 และกำหนดให้ task ทำงานพร้อมกัน
+# สร้าง ฟังชั่น main เพื่อกำหนด ตัวแปร task  กับ 2 และกำหนดให้ task ทำงานพร้อมกัน
+# ใช้ gather เพื่อเก็บ task เป็นลิสไว้เพื่อที่จะได้่ประหยัดบรรทัด
 async def main():
     task1 = asyncio.create_task(hello(1)) #returns immediately
 #, the task is created await asyncio.sleep(3)
     task2 = asyncio.create_task(hello(2))
-    await task1
-    await task2
+    await asyncio.gather(task1, task2)
 
 if __name__ == '__main__':
     start = time.time()
