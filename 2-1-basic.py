6d2eff92bfbd95a1183d8603289b45ffdd2a211b
pongsagul <6310301003@cdti.ac.th>
2023-07-26 14:49:08 +0700

    commit_1

---
+++ 2-1-basic.py
@@ -0,0 +1,23 @@
+import asyncio
+import time
+
+#สร้างฟังชั่น hello เพื่อปริ้นค่า แบบ asyncio
+async def hello(i):
+    print(f"{time.ctime()} hello {i} started")
+    await asyncio.sleep(4)
+    print(f"{time.ctime()} hello {i} done")
+
+
+#สร้าง ฟังชั่น main เพื่อกำหนด ตัวแปร task  กับ 2 และกำหนดให้ task ทำงานพร้อมกัน
+async def main():
+    task1 = asyncio.create_task(hello(1)) #returns immediately
+#, the task is created await asyncio.sleep(3)
+    task2 = asyncio.create_task(hello(2))
+    await task1
+    await task2
+
+if __name__ == '__main__':
+    start = time.time()
+    asyncio.run(main())
+    end = time.time()
+    print(f'Time: {end-start:.2f} sec')
