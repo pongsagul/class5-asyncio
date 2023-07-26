9db07d72f55458c64c3eeb94f4d0014cc5085406
pongsagul <6310301003@cdti.ac.th>
2023-07-26 14:16:19 +0700

    commit_3

--- 1-3-simple-async-right.py
+++ 1-3-simple-async-right.py
@@ -1,11 +1,13 @@
 import asyncio 
 import time
 
+#สร้าง ฟังชั่น sleep เพื่อเพื่อให้ปริ้นค่า time เริ่ม start ไปเรื่อยโดยมรทศนิยม 2 ตำแหน่ง
+#เปลี่ยนการเขียน asyncio ให้ถูกต้อง
 async def sleep():
     print (f'Time: {time.time() - start:.2f}')
     await asyncio.sleep(1)
 
-
+#สร้าง ฟังชั่น sum และสร้างตัวแปร total และค่า task a และ b และนำมาบวกกับ number
 async def sum (name, numbers):
     total = 0
     for number in numbers:
@@ -16,6 +18,7 @@ async def sum (name, numbers):
 
 start =  time.time()
 
+# สร้าง loop ขึ้นมา และก็สร้าง task ออกมา 2 task และก็ loop เข้าไปและก็ print ออกมา และก็ปิด loop
 loop = asyncio.get_event_loop()
 tasks = [
     loop.create_task (sum ("A", [1, 2])),
@@ -24,5 +27,6 @@ tasks = [
 loop.run_until_complete (asyncio.wait(tasks))
 loop.close()
 
+#output ออกมา เวลาจะต่างจาก ข้อ 2 จะเหลืออยู่ 3 วิ
 end =  time.time()
 print (f'Time: {end-start:.2f} sec') 
